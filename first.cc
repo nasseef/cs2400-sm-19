ba94025ffaf21b48482cef7481505b55b9f576c2
Nasseef Abukamail <abukamai@ohio.edu>
2019-01-18 11:42:22 -0500

    created first.cc example

---
+++ first.cc
@@ -0,0 +1,21 @@
+/*
+ *        File: first.cc
+ *      Author: Nasseef Abukamail
+ *        Date: January 18, 2019
+ * Description: Add Description
+ */
+
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+// function prototypes
+
+// main function
+int main() {
+    
+    cout << "Hello World!";
+    return 0;
+}  // main

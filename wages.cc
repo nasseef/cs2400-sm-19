034d699cbebb573dad7697b59b7caa38b7b0f435
Nasseef Abukamail <abukamai@ohio.edu>
2019-01-23 11:40:42 -0500

    Added the wages program

---
+++ wages.cc
@@ -0,0 +1,29 @@
+/*
+ *        File: wages.cc
+ *      Author: Nasseef Abukamail
+ *        Date: January 23, 2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+//function prototypes
+
+int main(int argc, char const *argv[]) {
+
+    //1, 2
+    double rate, hours;
+    hours = 30;
+    rate = 10;
+    //3 calculation
+    double wages;
+    wages = rate * hours;
+
+    //4 output
+    cout << "Wages = " << wages << endl;
+    
+    return 0;
+}// main

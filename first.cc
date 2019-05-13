c4a7c83b7202f3abfed72397689b863705aa7087
Nasseef Abukamail <abukamai@ohio.edu>
2019-05-13 13:41:02 -0400

    first program

---
+++ first.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: first.cc
+ *      Author: Nasseef Abukamail
+ *        Date: May 13, 2019
+ * Description: Output "Hello World!"
+ */
+
+#include <iostream>
+//#include <iomanip>
+//#include <cstdlib>
+
+using namespace std;
+
+// function prototypes
+
+int main(int argc, char const *argv[]) { //main
+
+    cout << "Hello World!" ;
+    return 0;
+}// main

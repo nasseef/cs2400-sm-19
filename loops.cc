efad652301335e70eed1503486e535355e69d84c
Nasseef Abukamail <abukamai@ohio.edu>
2019-05-22 13:33:53 -0400

    loop example

---
+++ loops.cc
@@ -0,0 +1,26 @@
+/*
+ *        File: loops.cc
+ *      Author: Nasseef Abukamail
+ *        Date: May 22, 2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+//function prototypes
+
+int main(int argc, char const *argv[]) {
+
+
+    int x = -15;
+    //counter loop
+    while (x > 0) {
+        cout << "Hello";
+        x--;
+    }
+
+    return 0;
+}// main
